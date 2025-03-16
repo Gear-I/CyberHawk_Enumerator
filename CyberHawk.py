889649e135a8b522bce8fd99595c37c729a0fdc9
Dielle De Noon <99276218+Gear-I@users.noreply.github.com>
2025-03-16 15:40:01 -0400

    Updated

--- CyberHawk.py
+++ CyberHawk.py
@@ -1,12 +1,13 @@
 from termcolor import colored
+from collections import deque
 import pyfiglet
 import time
 import requests
 import argparse
 import threading
-import queuelib
 
 
+    
 # ASCII title
 ascii_title = pyfiglet.figlet_format("""
  
@@ -30,27 +31,27 @@ def print_blinking_title():
         time.sleep(0.5)
 
 # Function to check if a directory exists
-def check_dir(base_url, directory, timeout,valid_dirs):
+def check_dir(base_url, directory, timeout,valid_dirs,lock):
     url = f"{base_url}/{directory}.html"
     try:
         response = requests.get(url, timeout=timeout)
         if response.status_code != 404:
-            valid_dirs.append(url)  # Store the valid directory URL
+            with lock:
+                valid_dirs.append(url)  # Store the valid directory URL
             print(f"[+] Valid directory found: {url} (Status: {response.status_code})")
     except requests.exceptions.RequestException:
         pass  # Ignore timeout and other connection issues
 
 # Worker function for threading
-def worker(base_url, q, timeout, thread_id,valid_dirs):
+def worker(base_url, dirs, timeout, thread_id,valid_dirs):
     print(f"[Thread {thread_id}] started") # Prints when thread starts
-    while not q.empty():
-        directory = q.get()
-        check_dir(base_url, directory, timeout, valid_dirs)
-        q.task_done()
+    while dirs():
+        directory = dirs.pop()
+        check_dir(base_url, directory, timeout, valid_dirs,lock)
         print(f"[Thread {thread_id}] finished") # Prints when thread finishes
 
 # Main function to handle arguments and threading
-def main():
+def argu():
     parser = argparse.ArgumentParser(description="Multi-Threaded Web Directory Enumerator")
 
     # CLI arguments
@@ -70,18 +71,16 @@ def main():
         return
 
 # Intialize the queue as a queuelib Queue
-    q = queuelib.Queue()
-    for directory in directories:
-        q.put(directory)
-
-        valid_dirs = [] # List to store valid directories
+    dirs = deque(directories)
+    valid_dirs = [] # List to store valid directories
+    lock = threading.Lock()
 
     print(f"[*] Scanning {args.target} with {args.threads} threads...")
 
     # Start threading
     threads = []
     for i in range(1, args.threads + 1):  # Thread IDs start from 1
-        t = threading.Thread(target=worker, args=(i, args.target, q, args.timeout, valid_dirs))
+        t = threading.Thread(target=worker, args=(i, args.target, dirs, args.timeout, valid_dirs, lock))
         t.start()
         threads.append(t)
 
@@ -102,6 +101,4 @@ def main():
     
     print("[*] Scan completed.")
 
-# Run the script
-if __name__ == "__main__":
-    main()
+
